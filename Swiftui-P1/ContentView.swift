2c11ec29e4e0f03ea3edadbfe07cfc2ce4baf555
Mohammed Alarbash <mohammed@arbash.net>
2020-05-01 22:25:11 +0300

    First Practice

--- Swiftui-P1/ContentView.swift
+++ Swiftui-P1/ContentView.swift
@@ -5,12 +5,26 @@
 //  Created by MOHD ALARBASH on 5/1/20.
 //  Copyright © 2020 MOHD ALARBASH. All rights reserved.
 //
+// Thanks :)
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("BACKGROUND")
+                .resizable()
+            VStack{
+                
+                Image("Header")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                Spacer()
+                Image("Mosque")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
